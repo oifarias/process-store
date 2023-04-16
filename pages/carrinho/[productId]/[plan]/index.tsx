8de08529702ed6923a66c4e05408c5c90835c4aa
oifarias <lucasfariaskate@gmail.com>
2023-04-16 14:28:07 -0300

    ProcesStoreStore

--- pages/carrinho/[productId]/[plan]/index.tsx
+++ pages/carrinho/[productId]/[plan]/index.tsx
@@ -27,53 +27,6 @@ interface PackageTierProps {
   typePlan: string;
   checked?: boolean;
 }
-const PackageTier = ({
-  title,
-  options,
-  typePlan,
-  checked = false,
-}: PackageTierProps) => {
-  const colorTextLight = checked ? "white" : "purple.600";
-  const bgColorLight = checked ? "purple.400" : "gray.300";
-  const colorTextDark = checked ? "white" : "purple.500";
-  const bgColorDark = checked ? "purple.400" : "gray.300";
-
-  return (
-    <Stack
-      p={3}
-      py={3}
-      justifyContent={{
-        base: "flex-start",
-        md: "space-around",
-      }}
-      direction={{
-        base: "column",
-        md: "row",
-      }}
-      alignItems={{ md: "center" }}
-    >
-      <Heading size={"md"}>{title}</Heading>
-      <List spacing={3} textAlign="start">
-        {options.map((desc, id) => (
-          <ListItem key={desc.id}>
-            <ListIcon as={FaCheckCircle} color="green.500" />
-            {desc.desc}
-          </ListItem>
-        ))}
-      </List>
-      <Heading size={"xl"}>{typePlan}</Heading>
-      <Stack>
-        <Button
-          size="md"
-          color={useColorModeValue(colorTextLight, colorTextDark)}
-          bgColor={useColorModeValue(bgColorLight, bgColorDark)}
-        >
-          Get Started
-        </Button>
-      </Stack>
-    </Stack>
-  );
-};
 
 const ThreeTierPricingHorizontal = () => {
   const router = useRouter();
@@ -101,38 +54,21 @@ const ThreeTierPricingHorizontal = () => {
 
   return (
     <Box py={6} px={5} display={"flex"} flexDirection={"column"}>
-      <Container maxW={"5xl"} mt={10}>
-        <Flex direction={"row"} gap={2}>
+      <Container maxW={"5xl"} mt={5}>
+        <Flex direction={"row"} gap={2} mb={'10px'}>
           <Title size="3em" title="Carrinho de compras" />
         </Flex>
-        <SimpleGrid
-          columns={{ base: 1, lg: 2 }}
-          spacing={{ base: 2, md: 2 }}
-          py={{ base: 5, md: 5 }}
-        >
-          {infos && (
-            <ProductSimple
-              name={infos.name}
-              mensal={infos.mensal}
-              isNew={infos.isNew}
-              imageURL={`../../${infos.imageURL}`}
-              id={infos.id}
-              description={infos.description}
-              key={infos.id + 100}
-            />
-          )}
-          <Box display={"flex"} justifyContent={"flex-end"} marginTop={""} >
-            <Button
-              w="full"
-              colorScheme="black"
-              bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
-              display={"flex"}
-              
-            >
-              Finalizar a compra
-            </Button>
-          </Box>
-        </SimpleGrid>
+        {infos && (
+          <ProductSimple
+            name={infos.name}
+            mensal={infos.mensal}
+            isNew={infos.isNew}
+            imageURL={`../../${infos.imageURL}`}
+            id={infos.id}
+            description={infos.description}
+            key={infos.id + 100}
+          />
+        )}
         <Divider></Divider>
       </Container>
       <Box p={4}>
